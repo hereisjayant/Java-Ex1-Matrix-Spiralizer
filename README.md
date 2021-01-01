f6fae025554263002eda29f8d62963484c4b7543
hereisjayant <hereisjayant@gmail.com>
2021-01-01 12:22:01 +0530

    Uploaded

---
+++ README.md
@@ -0,0 +1,50 @@
+Matrix Spiralizer
+===
+
+## Overview
+
+Given an `m x n` matrix of `int`s, implement a method that returns a new matrix with the contents that are a "spiralized" version of the original matrix.
+
+What we will call the "natural order" is one that involves reading a matrix row-by-row starting with the top row. The "spiral order" keeps the first row unchanged but then the subsequent elements follow a spiral, with the last element in the original matrix (the element at the bottom right) becoming the innermost element in a spiral. (The figure below has two examples of the natural vs. sprial order.)
+
+![Two examples natural vs. spiral order](img/Spiralizer.png "Example")
+
+## Your Task
+
+In the Java class `MatrixSpiralizer`, implement the method `int[][] spiralize(int[][])` that returns a matrix of identical dimensions to the input matrix, but with the contents in spiral order. Your implementation should account for matrices of different sizes and should handle corner cases as well (`0 x 0` matrices and `1 x 1` matrices, for example).
+
+## Examples
+
+The matrix
+
+| 1   | 2   | 3   |
+|:---:|:---:|:---:|
+| 4    | 5    | 6    |
+| 7    | 8    | 9    |
+
+would become
+
+| 1    | 2    | 3    |
+|:---:|:---:|:---:|
+| 8    | 9    | 4    |
+| 7    | 6    | 5    |
+
+after spiralizing.
+
+The matrix
+
+| 7    | 5    | 1    | 4    | 6    |
+|:---:|:---:|:---:|:---:|:---:|
+| 3    | 2    | 9    | 1    | 8    |
+| 8    | 3    | 2    | 4    | 1    |
+| 10   | 11   | 15   | 19   | 14   |
+
+would become
+
+| 7    | 5    | 1    | 4    | 6    |
+|:---:|:---:|:---:|:---:|:---:|
+| 4    | 1    | 10   | 11   | 3    |
+| 2    | 14   | 19   | 15   | 2    |
+| 3    | 8    | 8    | 1    | 9    |
+
+after spiralizing.
---
+++ src/main/java/spiralizer/MatrixSpiralizer.java
@@ -0,0 +1,121 @@
+package spiralizer;
+
+import org.jetbrains.annotations.NotNull;
+
+public class MatrixSpiralizer {
+
+    /**
+     * @param inMatrix
+     * @return a matrix that is "spriralized" version of inMatrix
+     */
+    public static int[][] spiralize(@NotNull int[][] inMatrix) {
+
+        int outMatrix[][];
+        int rows = inMatrix.length;
+        int cols = inMatrix[0].length;
+
+        outMatrix = new int[rows][cols];
+
+        int numberOfElements = rows*cols;
+
+        int nth_element= 0; //counter for inMatrix
+
+        int start_forward= 0;
+        int end_forward = cols-1;
+        int start_down =1;
+        int end_down = rows-1;
+        int start_backwards = end_forward-1;
+        int end_backwards = 0;
+        int start_upwards = end_down-1;
+        int end_upwards = start_down;
+
+        int i=0;
+        int j=0;
+
+        while(true){
+            //this goes forward
+            for(j= start_forward; j<=end_forward; j++){
+
+                //this gets the position of the input matrix
+                int row_in = getCurrentRow(nth_element, cols);
+                int col_in = getCurrentCol(nth_element, cols);
+
+                outMatrix[i][j]= inMatrix[row_in][col_in];
+                nth_element++;
+
+                if(nth_element==numberOfElements) break;
+            }
+            j--;
+            end_forward--;
+            start_forward++;
+            if(nth_element==numberOfElements) break;
+
+            //this goes down
+            for(i= start_down; i<=end_down; i++){
+
+                //this gets the position of the input matrix
+                int row_in = getCurrentRow(nth_element, cols);
+                int col_in = getCurrentCol(nth_element, cols);
+
+                outMatrix[i][j]= inMatrix[row_in][col_in];
+                nth_element++;
+
+                if(nth_element==numberOfElements) break;
+            }
+            i--;
+            end_down--;
+            start_down++;
+            if(nth_element==numberOfElements) break;
+
+            //this goes backwards
+            for(j= start_backwards; j>=end_backwards; j--){
+
+                //this gets the position of the input matrix
+                int row_in = getCurrentRow(nth_element, cols);
+                int col_in = getCurrentCol(nth_element, cols);
+
+                outMatrix[i][j]= inMatrix[row_in][col_in];
+                nth_element++;
+
+                if(nth_element==numberOfElements) break;
+            }
+            j++;
+            end_backwards++;
+            start_backwards--;
+            if(nth_element==numberOfElements) break;
+
+            //this goes upwards
+            for(i= start_upwards; i>=end_upwards; i--){
+
+                //this gets the position of the input matrix
+                int row_in = getCurrentRow(nth_element, cols);
+                int col_in = getCurrentCol(nth_element, cols);
+
+                outMatrix[i][j]= inMatrix[row_in][col_in];
+                nth_element++;
+
+                if(nth_element==numberOfElements) break;
+            }
+            i++;
+            end_upwards++;
+            start_upwards--;
+            if(nth_element==numberOfElements) break;
+        }
+
+        
+
+        return outMatrix;
+    }
+
+
+    private static int getCurrentRow(int n, int rows){
+
+        return n/rows;
+    }
+
+    private static int getCurrentCol(int n, int rows){
+
+        return n%rows;
+    }
+
+}
